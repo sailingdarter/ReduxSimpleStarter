2baf4efc86276137959bf37d65efad71b551098b
Brian Armstrong <brianarms@gmail.com>
2018-03-26 21:49:46 -0500

    Finished through the end of Lecture 26 in course. However Video thumbnails are not working at this time. Troubleshoot this!!!!!

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -4,7 +4,7 @@ import VideoListItem from './listItem';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   });
   return (
     <ul className="col-md-4 list-group">
