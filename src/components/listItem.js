625264145e90f47a604781f11cea98ff5bd59062
Brian Armstrong <brianarms@gmail.com>
2018-03-27 21:06:12 -0500

    List item

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -8,7 +8,7 @@ const VideoListItem = ({video}) => {
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={imageUrl} />
+          <img className="media-object" src={ imageUrl } />
         </div>
         <div className="media-body">
           <div className="media-heading">{video.snippet.title}</div>
