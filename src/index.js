aca8d611b906d4afb530762c0a73f7a336b86bb1
Brian Armstrong <brianarms@gmail.com>
2018-03-30 05:39:30 -0500

    Almost Finished React

--- src/index.js
+++ src/index.js
@@ -15,22 +15,32 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
+    this.videoSearch('surfboards');
+
+  }
+
+  videoSearch(term){
     YTSearch({key: API_KEY,  term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      // this.setState.({video: videos}) is a more expanded form of showin the iteration over a video list
-
+      this.setState({
+        videos: videos,             // this.setState.({video: videos}) is a more expanded form of showin the iteration over a video list
+        selectedVideo: videos[0]
+      });
     });
-
   }
 
   render() {
     return(
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
